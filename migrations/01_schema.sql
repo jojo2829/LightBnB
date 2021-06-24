1cfc5f3fae63ca879391469b02375400fb754286
jojo2829 <joulinli@gmail.com>
2021-06-24 00:31:05 +0000

    update to compass exact code to be able to import seed data

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -18,18 +18,18 @@ CREATE TABLE properties (
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-  cost_per_night INTEGER NOT NULL DEFAULT 0,
-  parking_spaces INTEGER NOT NULL DEFAULT 0,
-  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
-  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
 
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
-  postal_code VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
 
-  is_active BOOLEAN DEFAULT TRUE
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
@@ -42,8 +42,9 @@ CREATE TABLE reservations (
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  message TEXT,
-  rating SMALLINT NOT NULL DEFAULT 0,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
-  CHECK (rating >= 0 AND rating <= 5)
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
 );
